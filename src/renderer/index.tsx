b6354099f7656ea1597bb8abd1e4b0cb9a6abc92
mwingfield <mwingfie@gmail.com>
2019-11-05 10:36:47 -0700

    react-apollo at 2.5 works

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,23 +1,11 @@
 'use strict';
-import 'spectre.css/dist/spectre.min.css';
-import 'spectre.css/dist/spectre-exp.min.css';
-import 'spectre.css/dist/spectre-icons.min.css';
 import React from "react";
 import ReactDOM from "react-dom";
-import {ApolloProvider, useQuery} from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {HttpLink} from "apollo-link-http";
 import {ApolloClient} from "apollo-client";
-
-const {ipcRenderer} = require('electron');
-
-ipcRenderer.on('check-for-update-reply', (_event: any, arg: any) => {
-    console.log(arg);
-});
-ipcRenderer.on('update-downloaded', (event: any, arg: any) => {
-    console.log("Client update downloaded:", event, arg);
-});
+import {ApolloProvider, Query} from 'react-apollo';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
@@ -38,9 +26,18 @@ const GET_USERS = gql`
 `;
 
 const Test = () => {
-    const {data} = useQuery(GET_USERS);
-    console.log(data);
-    return null
+
+    return (<div>
+        <Query query={GET_USERS}>
+            {
+                // @ts-ignore
+                ({data, loading, error}) => {
+                if (error) return (<h1>Error: {JSON.stringify(error)}</h1>);
+                if (loading) return (<h1>Loading...</h1>);
+                return (<h1>Data: {JSON.stringify(data)}</h1>);
+            }}
+        </Query>
+    </div>)
 };
 
 ReactDOM.render(<ApolloProvider client={client}><Test/></ApolloProvider>, document.getElementById("app"));
