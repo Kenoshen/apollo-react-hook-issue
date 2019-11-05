c5fe0fb4c3d5d1728f249d662041f4b00e69f4fe
mwingfield <mwingfie@gmail.com>
2019-11-05 11:11:01 -0700

    switched to using apollo's own getting started example

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,15 +1,15 @@
-'use strict';
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+
+import {ApolloClient} from "apollo-client";
 import gql from "graphql-tag";
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {HttpLink} from "apollo-link-http";
-import {ApolloClient} from "apollo-client";
 import {ApolloProvider, Query} from 'react-apollo';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-    uri: 'http://localhost:9001/'
+    uri: "https://48p1r2roz4.sse.codesandbox.io"
 });
 
 const client = new ApolloClient({
@@ -17,27 +17,43 @@ const client = new ApolloClient({
     link
 });
 
-const GET_USERS = gql`
-    query GetUsers {
-        getUsers {
-            id
-        }
-    }
-`;
+function ExchangeRates() {
+    return (<Query query={gql`
+      {
+          rates(currency: "USD") {
+              currency
+              rate
+          }
+      }
+  `}>
+        {
+            // @ts-ignore
+            ({data, loading, error}) => {
+                if (loading) return <p>Loading...</p>;
+                if (error) return <p>Error :(</p>;
 
-const Test = () => {
-
-    return (<div>
-        <Query query={GET_USERS}>
-            {
                 // @ts-ignore
-                ({data, loading, error}) => {
-                if (error) return (<h1>Error: {JSON.stringify(error)}</h1>);
-                if (loading) return (<h1>Loading...</h1>);
-                return (<h1>Data: {JSON.stringify(data)}</h1>);
+                return data.rates.map(({ currency, rate }) => (
+                    <div key={currency}>
+                        <p>
+                            {currency}: {rate}
+                        </p>
+                    </div>
+                ));
             }}
-        </Query>
-    </div>)
-};
+    </Query>);
+}
 
-ReactDOM.render(<ApolloProvider client={client}><Test/></ApolloProvider>, document.getElementById("app"));
+// @ts-ignore
+const App = () => (
+    <ApolloProvider
+        // @ts-ignore
+        client={client}>
+        <div>
+            <h2>My first Apollo app 🚀</h2>
+            <ExchangeRates />
+        </div>
+    </ApolloProvider>
+);
+
+render(<App />, document.getElementById("app"));
